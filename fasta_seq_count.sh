2e6d50e3f5d019a3d7894b006af59d091481a181
danielwooseokdo <156361130+danielwooseokdo@users.noreply.github.com>
2024-01-23 18:13:52 -0500

    Add files via upload

---
+++ fasta_seq_count.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This script can count the number of sequences in a fasta file."
+greeting="Hello"
+your_name="Daniel"
+echo "<"$greeting"><"$your_name">!"
+mkdir /home/daniel/Desktop/bioinformatics
+mkdir /home/daniel/Desktop/bioinformatics/learning_bash
+mv transcriptome.fasta /home/daniel/Desktop/bioinformatics/learning_bash
+cd /home/daniel/Desktop/bioinformatics/learning_bash
+grep -c ">" transcriptome.fasta > "fasta_count.txt"
